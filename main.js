2c4146857eba42e347f6bf40f15e08214403d0e2
Sid Ghimire <siddharthaghimire@gmail.com>
2021-06-12 20:47:27 +0545

    update

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     var file = document.getElementById("uploadFile");
     var canvas = document.getElementById("mainCanvas");
     var context = canvas.getContext("2d");
+    context.translate(0, 0);
+
 
     $("#uploadFile").change(function(e) {
         e.preventDefault();
@@ -41,6 +43,153 @@ $(document).ready(function() {
         }
 
     });
+    var action;
+    $("#drawButton").on('click', function() {
+        action = 'draw';
+    });
+    $("#brush").on('click', function() {
+        action = 'brush';
+    });
+    $("#crop").on('click', function() {
+        action = 'crop';
+    });
+    $("#bucket").on('click', function() {
+        if (action == 'crop') {
+            context.fillStyle = "#FF0000";
+            context.fillRect(cropXInit - 1, cropYInit - 1, cropDimX + 2, cropDimY + 2);
+
+        }
+    });
+    var change = 0;
+    var newX = 0;
+    var newY = 0;
+
+    $("#delete").on("click", function() {
+        if (action == 'crop') {
+            context.clearRect(cropXInit - 1, cropYInit - 1, cropDimX + 2, cropDimY + 2);
+        }
+
+    });
+    $("#rotateClock").on('click', function() {
+        var tempCanvas = document.createElement("canvas");
+        var tempCtx = tempCanvas.getContext("2d");
+        tempCanvas.width = canvas.width;
+        tempCanvas.height = canvas.height;
+        tempCtx.drawImage(canvas, 0, 0, tempCanvas.width, tempCanvas.height);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        canvas.width = tempCanvas.height;
+        canvas.height = tempCanvas.width;
+        context.save();
+
+        context.translate(canvas.width / 2, canvas.height / 2);
+        context.imageSmoothingEnabled = false;
+
+        context.rotate(-90 * Math.PI / 180);
+
+        context.drawImage(tempCanvas, -canvas.width / 2, -canvas.height / 2);
+        context.restore();
+
+        tempCanvas.remove();
+    });
+    $("#rotateAntiClock").on('click', function() {
+        var tempCanvas = document.createElement("canvas");
+        var tempCtx = tempCanvas.getContext("2d");
+        tempCanvas.width = canvas.width;
+        tempCanvas.height = canvas.height;
+        tempCtx.drawImage(canvas, 0, 0, tempCanvas.width, tempCanvas.height);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        canvas.width = tempCanvas.height;
+        canvas.height = tempCanvas.width;
+        context.save();
+        context.translate(canvas.width / 2, canvas.height / 2);
+        context.imageSmoothingEnabled = false;
+
+        context.rotate(90 * Math.PI / 180);
+        context.drawImage(tempCanvas, -canvas.width / 2, -canvas.height / 2);
+        context.restore();
+
+        tempCanvas.remove();
+
+    });
+
+    var x = 0;
+    var y = 0;
+    var cropXInit = 0;
+    var cropYInit = 0;
+    var cropDimX = 0;
+    var cropDimY = 0;
+    var isDrawing = false;
+    var isBrushing = false;
+    canvas.addEventListener("mousedown", e => {
+
+        if (action == 'draw') {
+            isDrawing = true;
+            x = e.offsetX;
+            y = e.offsetY;
+        } else if (action == 'brush') {
+            isBrushing = 'true';
+            x = e.offsetX;
+            y = e.offsetY;
+        } else if (action == 'crop') {
+
+            x = e.offsetX;
+            y = e.offsetY;
+        }
+    });
+    canvas.addEventListener("mouseup", e => {
+
+        if (action == 'draw') {
+            isDrawing = false;
+        } else if (action == 'brush') {
+            isBrushing = false;
+        } else if (action == 'crop') {
+            var tempCanvas = document.createElement("canvas");
+            var tempCtx = tempCanvas.getContext("2d");
+            tempCanvas.width = canvas.width;
+            tempCanvas.height = canvas.height;
+            tempCtx.drawImage(canvas, 0, 0, tempCanvas.width, tempCanvas.height);
+
+            context.lineWidth = 1;
+            context.strokeRect(x, y, e.offsetX - x, e.offsetY - y);
+            cropXInit = x;
+            cropYInit = y;
+            cropDimX = e.offsetX - x;
+            cropDimY = e.offsetY - y;
+        }
+
+
+    });
+    canvas.addEventListener("mousemove", e => {
+
+        context.translate(0, 0);
+        if (isDrawing) {
+            context.beginPath();
+            context.lineCap = 'round';
+            console.log(x, y, e.offsetX, e.offsetY);
+            context.moveTo(x, y);
+            context.lineTo(e.offsetX, e.offsetY);
+            x = e.offsetX;
+            y = e.offsetY;
+            console.log(e.offsetX, e.offsetY);
+            context.lineWidth = 3;
+            context.stroke();
+            context.closePath();
+        } else if (isBrushing) {
+            context.beginPath();
+            context.lineCap = 'round';
+            context.moveTo(x, y);
+            context.lineTo(e.offsetX, e.offsetY);
+            x = e.offsetX;
+            y = e.offsetY;
+            context.lineWidth = 10;
+            context.stroke();
+            context.closePath();
+
+        }
+
+    });
+
+
 
     download_img = function(el) {
         var image = canvas.toDataURL("image/jpg");
